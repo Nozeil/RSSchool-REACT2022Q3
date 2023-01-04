b4ab00f9441fd41dbd5bc0025eccbae8d4f7306f
Artur Voloshin <nozeilwrk@gmail.com>
2023-01-04 13:43:00 +0300

    feat: implement gh-pages deploy

--- react-redux/src/index.tsx
+++ react-redux/src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './app/App';
 import './index.css';
 import { Provider } from 'react-redux';
@@ -10,9 +10,9 @@ const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </Provider>
   </React.StrictMode>
 );
